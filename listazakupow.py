1740a568b66f3dbbfabb486eb3060fadde1f5157
Patryk Pabisiak <patryk.pabisiak1908@gmail.com>
2021-01-10 02:28:09 +0100

    Add for loop in lista

--- listazakupow.py
+++ listazakupow.py
@@ -1,2 +1,8 @@
 shops = { 'piekarnia': ['Chleb ' , 'Pączek ' , 'Bułka ' ], 'warzywniak' : [ 'Marchew ' , 'Seler ' , 'Rukola ']       
 }
+
+for key, value in shops.items():
+        print (f'Idę do sklepu {key} i kupuję tu następujące rzeczy: {value}')
+
+ctr = sum(map(len, shops.values()))
+print("W sumie kupuję ",ctr, "produktów" )
